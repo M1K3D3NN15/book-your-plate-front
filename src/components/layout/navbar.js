f9e29a64ed652b239349f7bbb3b1b2eb590db87f
marcotribuzio <marco.tribuz@gmail.com>
2020-09-04 12:00:08 +0200

    misc

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -14,7 +14,28 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
-}));
+})/*
+class Navbar extends React.Component {
+
+  render() {
+    const classes = useStyles();
+
+    return (
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      )
+  }
+}*/
+);
 
 
 function Navbar () {
